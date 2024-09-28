ba1c6abb3dd5b4151f7c391e1f717022cb1d48c7
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-09-28 17:02:42 -0600

    Profile done and Write data half done

--- app/src/main/java/com/example/datastructurevisualizerapp/screens/profile.kt
+++ app/src/main/java/com/example/datastructurevisualizerapp/screens/profile.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.ButtonDefaults
@@ -31,6 +32,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -98,7 +101,9 @@ fun userProfileScreen(
         OutlinedTextField(
             value = password,
             onValueChange = { /*TODO*/},
+            visualTransformation = PasswordVisualTransformation(),
             label = { Text("Contraseña") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
         Spacer(modifier = Modifier.size(32.dp))
         FilledTonalButton(
--- app/src/main/java/com/example/datastructurevisualizerapp/screens/writeData.kt
+++ app/src/main/java/com/example/datastructurevisualizerapp/screens/writeData.kt
@@ -1,2 +1,47 @@
 package com.example.datastructurevisualizerapp.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.datastructurevisualizerapp.R
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WriteData(){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            stringResource(R.string.insert_data),
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.black),
+        )
+        Text(
+            stringResource(R.string.data_examples),
+            fontWeight = FontWeight.Thin,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+
+        //OutlinedTextField(value = , onValueChange = )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewWriteData(){
+    WriteData()
+}
